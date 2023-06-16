b8ecbb2082e44a767e3fffdb2e3fdf5ad2b69cc5
Joy Sutradhar <joysutradharaj@gmail.com>
2023-06-16 13:35:59 +0600

    login form completed

--- src/Pages/LoginRegister/Login.jsx
+++ src/Pages/LoginRegister/Login.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import img from "../../assets/5172658.jpg";
+import Form from "./Loginform";
+import useTitle from "../Hooks/useTitle";
 
 const Login = () => {
-    return (
-        <div>
-            Lets register
+    useTitle("Login");
+  return (
+    <div
+      style={{ backgroundImage: `url(${img})` }}
+      className=" bg-cover bg-no-repeat px-20"
+    >
+        <div className="py-20 mx-60 relative left-44">
+            <Form/>
         </div>
-    );
+        
+    </div>
+  );
 };
 
 export default Login;
